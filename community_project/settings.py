1959478b4f68aa02e7a2b99cd0ebe3ec8c37306e
DeokHyeon An <serenitywish@naver.com>
2022-05-05 10:51:52 +0900

    test5

--- community_project/settings.py
+++ community_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -168,3 +168,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "serenitywish91@gmail.com"
 EMAIL_HOST_PASSWORD = "zmvkmpxrsacbjqic"
+
+django_heroku.settings(locals())
